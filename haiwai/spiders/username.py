324da0a20f57a1a5b5a8e14b2d572007583a9e65
caiwenming <caiwenming@myhexin.com>
2019-04-12 16:10:49 +0800

    add file

--- haiwai/spiders/username.py
+++ haiwai/spiders/username.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+import json
 
 class UsernameSpider(scrapy.Spider):
     name = 'username'
     allowed_domains = ['www.stocktwits.com']
-    start_urls = ['http://www.stocktwits.com/']
+    start_urls = ['https://api.stocktwits.com/api/2/streams/symbol/AAPL.json?filter=top']
 
     def parse(self, response):
-        pass
+        print('...............................', json.loads(response.body)['response'])
