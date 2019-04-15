ffaf8f66cbbe1051b9360e7b930389d185272c26
caiwenming <caiwenming@myhexin.com>
2019-04-15 14:31:24 +0800

    add ua

--- haiwai/settings.py
+++ haiwai/settings.py
@@ -22,12 +22,12 @@ NEWSPIDER_MODULE = 'haiwai.spiders'
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 32
+# CONCURRENT_REQUESTS = 32
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 1.0
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -43,8 +43,8 @@ DEFAULT_REQUEST_HEADERS = {
   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
   'Accept-Language': 'en',
   'Connection': 'Keep-Alive',
-  'Accept-Encoding': 'gzip, deflate',
   'Accept-Language': 'en-US,*',
+  'User-Agen':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36',
 }
 
 # Enable or disable spider middlewares
