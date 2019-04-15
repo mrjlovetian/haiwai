e0c4c36358afe2087f704dfea7c70a5c25031fe1
caiwenming <caiwenming@myhexin.com>
2019-04-15 14:49:03 +0800

    add ua

--- haiwai/settings.py
+++ haiwai/settings.py
@@ -44,6 +44,7 @@ DEFAULT_REQUEST_HEADERS = {
   'Accept-Language': 'en',
   'Connection': 'Keep-Alive',
   'Accept-Language': 'en-US,*',
+  'Origin':'https://www.stocktwits.com',
   'User-Agen':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36',
 }
 
